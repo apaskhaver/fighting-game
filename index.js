ec256a48849f179cbbf2241e86aa8bec5f6016fe
Alexandra Paskhaver <apaskhaver@alexandras-mbp.myfiosgateway.com>
2022-05-27 15:34:53 -0400

    :zap: Adjust canvas coordinates, dimensions, and color

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ const canvasContext = canvas.getContext("2d");
 // adjust size of canvas to fit standard computer screens
 canvas.width = 1024;
 canvas.height = 576;
+
+canvasContext.fillRect(
+  (x = 0),
+  (y = 0),
+  (w = canvas.width),
+  (h = canvas.height)
+);
