6f4c7d59967e9924e0ee6d06b164d361d6add1ae
Alexandra Paskhaver <apaskhaver@alexandras-mbp.myfiosgateway.com>
2022-05-27 16:05:13 -0400

    :zap: Make basic Sprite class and render as rectangle

--- index.js
+++ index.js
@@ -5,9 +5,35 @@ const canvasContext = canvas.getContext("2d");
 canvas.width = 1024;
 canvas.height = 576;
 
+// adjust canvas color, dimensions, and coordinates
 canvasContext.fillRect(
   (x = 0),
   (y = 0),
   (w = canvas.width),
   (h = canvas.height)
 );
+
+class Sprite {
+  constructor(position) {
+    this.position = position;
+  }
+
+  draw() {
+    // color
+    canvasContext.fillStyle = "red";
+    // dimensions and coors
+    canvasContext.fillRect(
+      (x = this.position.x),
+      (y = this.position.y),
+      (w = 50),
+      (h = 50)
+    );
+  }
+}
+
+const player = new Sprite({
+  x: 0,
+  y: 0,
+});
+player.draw();
+console.log(player);
