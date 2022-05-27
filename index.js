5c723c743cb8326870172805585732d21994a3aa
Alexandra Paskhaver <apaskhaver@alexandras-mbp.myfiosgateway.com>
2022-05-27 15:31:21 -0400

    :zap: Adjust canvas size

---
+++ index.js
@@ -0,0 +1,6 @@
+const canvas = document.querySelector("canvas");
+const canvasContext = canvas.getContext("2d");
+
+// adjust size of canvas to fit standard computer screens
+canvas.width = 1024;
+canvas.height = 576;
