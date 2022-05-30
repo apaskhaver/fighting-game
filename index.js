b847ebf860b3860e4da4affbbc5f4d17751c5fce
apaskhaver <alexandra.paskhaver@gmail.com>
2022-05-30 15:53:29 -0400

    :hammer: Put collision logic into separate method

--- index.js
+++ index.js
@@ -154,6 +154,18 @@ const keys = {
   },
 };
 
+function rectangularCollision({ rectangle1, rectangle2 }) {
+  return (
+    rectangle1.attackBox.position.x + rectangle1.attackBox.width >=
+      rectangle2.position.x &&
+    rectangle1.attackBox.position.x <=
+      rectangle2.position.x + rectangle2.width &&
+    rectangle1.attackBox.position.y + rectangle1.attackBox.height >=
+      rectangle2.position.y &&
+    rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height
+  );
+}
+
 // what to do in every frame of animation loop
 function animate() {
   window.requestAnimationFrame(animate);
@@ -191,10 +203,10 @@ function animate() {
 
   // detect for collisions and attack state
   if (
-    player.attackBox.position.x + player.attackBox.width >= enemy.position.x &&
-    player.attackBox.position.x <= enemy.position.x + enemy.width &&
-    player.attackBox.position.y + player.attackBox.height >= enemy.position.y &&
-    player.attackBox.position.y <= enemy.position.y + enemy.height &&
+    rectangularCollision({
+      rectangle1: player,
+      rectangle2: enemy,
+    }) &&
     player.isAttacking
   ) {
     player.isAttacking = false;
