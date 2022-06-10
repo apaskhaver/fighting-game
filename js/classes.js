af6bf02c154631ca8549eac2e36438c126d75258
apaskhaver <alexandra.paskhaver@gmail.com>
2022-06-10 09:00:31 -0400

    :hammer: Keep code for attack box visual, comment it out. Just in case

--- js/classes.js
+++ js/classes.js
@@ -103,6 +103,14 @@ class Fighter extends Sprite {
     this.attackBox.position.x = this.position.x + this.attackBox.offset.x;
     this.attackBox.position.y = this.position.y + this.attackBox.offset.y;
 
+    // draw attack box
+    // canvasContext.fillRect(
+    //   this.attackBox.position.x,
+    //   this.attackBox.position.y,
+    //   this.attackBox.width,
+    //   this.attackBox.height
+    // );
+
     // move left by adjusting position x by velocity x
     this.position.x += this.velocity.x;
 
